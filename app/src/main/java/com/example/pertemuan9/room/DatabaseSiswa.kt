c3f0e31e64306e4db5b37e5fc10f62f93c025d8c
msidiq2235 <muhsidiq2005@gmail.com>
2025-11-28 15:33:39 +0700

    Membuat class DatabaseSiswa

---
+++ app/src/main/java/com/example/pertemuan9/room/DatabaseSiswa.kt
@@ -0,0 +1,11 @@
+package com.example.pertemuan9.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Siswa::class], version = 1, exportSchema = false)
+abstract class DatabaseSiswa : RoomDatabase(){
+
+}
