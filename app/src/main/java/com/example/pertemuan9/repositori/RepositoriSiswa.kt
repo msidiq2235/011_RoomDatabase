782db8e392c2a844452635e6c768f50ecac66757
msidiq2235 <muhsidiq2005@gmail.com>
2025-11-28 15:41:11 +0700

    Membuat class OfflineRepositoriSiswa

--- app/src/main/java/com/example/pertemuan9/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/pertemuan9/repositori/RepositoriSiswa.kt
@@ -8,3 +8,8 @@ interface RepositoriSiswa{
     fun getAllSiswaStream(): Flow<List<Siswa>>
     suspend fun insertSiswa(siswa: Siswa)
 }
+
+class OfflineRepositoriSiswa(
+    private val siswaDao: SiswaDao
+): RepositoriSiswa {
+}
