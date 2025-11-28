8fcfdb81dc9a31e6d92bd20402ff8f6be511ea60
msidiq2235 <muhsidiq2005@gmail.com>
2025-11-28 15:34:28 +0700

    Menambahkan companion object + Instance

--- app/src/main/java/com/example/pertemuan9/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/pertemuan9/room/DatabaseSiswa.kt
@@ -8,4 +8,9 @@ import androidx.room.RoomDatabase
 @Database(entities = [Siswa::class], version = 1, exportSchema = false)
 abstract class DatabaseSiswa : RoomDatabase(){
     abstract fun SiswaDao() : SiswaDao
+
+    companion object{
+        @Volatile
+        private var Instance: DatabaseSiswa? = null
+    }
 }
