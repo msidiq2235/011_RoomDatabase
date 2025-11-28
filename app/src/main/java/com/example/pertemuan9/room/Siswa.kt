997468d6a2de8b2ee248c5cf09736f028feefbb6
msidiq2235 <muhsidiq2005@gmail.com>
2025-11-28 15:15:34 +0700

    membuat file Siswa.kt dan data class siswa

---
+++ app/src/main/java/com/example/pertemuan9/room/Siswa.kt
@@ -0,0 +1,9 @@
+package com.example.pertemuan9.room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tblSiswa")
+data class Siswa (
+    @PrimaryKey(autoGenerate = true)
+)
