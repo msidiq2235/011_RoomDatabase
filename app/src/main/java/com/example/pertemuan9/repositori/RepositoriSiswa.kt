fd52128de5b3bbb2da96f564728355f6c82794bd
msidiq2235 <muhsidiq2005@gmail.com>
2025-12-05 14:58:04 +0700

    menambah 3 override di RepositoriSiswa.kt

--- app/src/main/java/com/example/pertemuan9/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/pertemuan9/repositori/RepositoriSiswa.kt
@@ -18,4 +18,8 @@ class OfflineRepositoriSiswa(
 ): RepositoriSiswa {
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+    //Tambah 3 override
+    override fun getSiswaStream(id: Int): Flow<Siswa?> =siswaDao.getSiswa(id)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
 }
