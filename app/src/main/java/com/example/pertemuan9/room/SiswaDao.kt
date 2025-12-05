56621c0d8bd547515222cffc800916264c364411
msidiq2235 <muhsidiq2005@gmail.com>
2025-12-05 20:53:55 +0700

    mengubah SiswaDao.kt

--- app/src/main/java/com/example/pertemuan9/room/SiswaDao.kt
+++ app/src/main/java/com/example/pertemuan9/room/SiswaDao.kt
@@ -10,19 +10,18 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface SiswaDao {
-    @Query(value = "SELECT * from tblSiswa ORDER BY nama ASC")
+    @Query("SELECT * from tblSiswa ORDER BY nama ASC")
     fun getAllSiswa(): Flow<List<Siswa>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa: Siswa)
 
-    // Tambahan 3 Fungsi
-    @Query(value = "SELECT * from tblSiswa WHERE id = :id")
+    @Query("SELECT * from tblSiswa WHERE id = :id")
     fun getSiswa(id: Int): Flow<Siswa>
-
-    @Delete
-    suspend fun delete(siswa: Siswa)
 
     @Update
     suspend fun update(siswa: Siswa)
+
+    @Delete
+    suspend fun delete(siswa: Siswa)
 }
